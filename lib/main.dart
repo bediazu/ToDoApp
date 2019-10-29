f0a7eeeb1a172e850126f74a564dea0fc321ccd2
Bruno <bruno.diaz.u@mail.pucv.cl>
2019-10-29 19:26:37 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import './models/global.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(50.0),
+            child: AppBar(
+              automaticallyImplyLeading: false,
+              titleSpacing: 0.0,
+              elevation: 0.0,
+              title: Row(
+                mainAxisSize: MainAxisSize.min,
+              ),
+              backgroundColor: Colors.red,
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.account_circle)),
+                  Tab(icon: Icon(Icons.add_a_photo))
+                ],
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                indicatorColor: Colors.transparent,
+              ),
+            ),
+          ),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                color: darkGreyColor,
+                child: TabBarView(
+                  children: <Widget>[
+                    Icon(Icons.account_circle),
+                    Icon(Icons.add_a_photo)
+                  ],
+                ),
+              ),
+              Container(
+                height: 120,
+                padding: EdgeInsets.only(left: 50),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50))),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'ToDoApp',
+                      style: titleTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: FloatingActionButton(
+                  onPressed: () => {},
+                  child: Container(child: Icon(Icons.add, size: 30)),
+                  backgroundColor: Colors.orange,
+                ),
+                padding: EdgeInsets.only(
+                    top: 90,
+                    left: MediaQuery.of(context).size.width * 0.5 - 20),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/global.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+
+TextStyle titleTextStyle = new TextStyle(fontFamily: 'Avenir',fontSize: 35 ,fontWeight: FontWeight.bold, color: darkGreyColor);
